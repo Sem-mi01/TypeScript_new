f2dfd6c84781dd3b9716280b6642513799d7d75c
Sem-mi01 <semmik.ys@gmail.com>
2025-08-13 18:19:37 +0400

    add homework4. made task4-1.html, task4_1.ts

---
+++ homework/homework4/homework4-1/task4_1.ts
@@ -0,0 +1,11 @@
+// #I2XsG6f
+//
+// – створити функцію, яка обчислює та повертає площу прямокутника зі сторонами  а і б
+
+function square (aNum:number, bNum:number) {
+    const result = aNum * bNum;
+    console.log(result);
+    return result;
+}
+
+    square(20, 30)
