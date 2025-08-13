d19693d2dd829d9f6ac9e04d07cdbd5a2a09d5e6
Sem-mi01 <semmik.ys@gmail.com>
2025-08-14 00:40:29 +0400

    add homework5, task5-1.html, task5_1.ts

---
+++ homework/homework5/homework5-1/task5_1.ts
@@ -0,0 +1,11 @@
+// #I2XsG6f
+//
+// – створити стрілочну функцію, яка обчислює та повертає площу прямокутника зі сторонами  а і б
+
+// @ts-ignore
+const square = (a: number, b: number): number => {
+    const result = a * b;
+    console.log(result);
+    return result;
+}
+console.log(square(20, 30));
