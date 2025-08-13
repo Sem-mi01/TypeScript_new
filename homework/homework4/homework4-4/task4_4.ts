9b35ac0de95e46a707b19b9f992dc7280115e054
Sem-mi01 <semmik.ys@gmail.com>
2025-08-13 18:38:10 +0400

    made task4-4.html, task4_4.ts

---
+++ homework/homework4/homework4-4/task4_4.ts
@@ -0,0 +1,11 @@
+// #SIdMd0hQ
+//
+// – створити функцію, яка приймає масив та виводить кожен його елемент
+
+    function printerArray(array: any[]): void {
+    for (const product of array) {
+        console.log(product);
+    }
+}
+    printerArray(array)
+
