813a8b986ba7f50a4c3881e55298dd57a49d10c4
Sem-mi01 <semmik.ys@gmail.com>
2025-08-13 18:26:57 +0400

    made task4-2.html, task4_2.ts

---
+++ homework/homework4/homework4-2/task4_2.ts
@@ -0,0 +1,11 @@
+// #ETGAxbEn8l
+//
+// – створити функцію, яка обчислює та повертає площу кола з радіусом r
+
+function roudSquare (radius:number): number {
+    const result = Math.PI * radius * radius;
+    console.log(result);
+    return result;
+}
+
+    roudSquare (5)
