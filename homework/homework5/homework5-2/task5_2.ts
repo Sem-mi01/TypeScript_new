588749b0e5fbbff7e9354166ec4bade16d8d3ec8
Sem-mi01 <semmik.ys@gmail.com>
2025-08-14 13:44:05 +0400

    add homework5, task5-2.html, task5_2.ts

---
+++ homework/homework5/homework5-2/task5_2.ts
@@ -0,0 +1,11 @@
+// #ETGAxbEn8l
+//
+// – створити стрілочну функцію, яка обчислює та повертає площу кола з радіусом r
+
+
+// @ts-ignore
+const roudSquare = (radius: number): number => {
+    return Math.PI * radius * radius;
+}
+
+console.log(roudSquare(5));
