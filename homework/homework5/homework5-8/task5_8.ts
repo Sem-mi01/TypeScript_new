38f946c7ef4360500f75b5e038379d824919eaab
Sem-mi01 <semmik.ys@gmail.com>
2025-08-14 15:36:31 +0400

    add homework5, task5-8.html, task5_8.ts

---
+++ homework/homework5/homework5-8/task5_8.ts
@@ -0,0 +1,13 @@
+
+// – створити стрілочну функцію, яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список (ul li) та виводить його через document.write
+
+type PrimElement = string | number | boolean
+// @ts-ignore
+const primElements = (arrayElements: PrimElement[]): void => {
+    document.write('<ul>');
+    for (const element of arrayElements) {
+        document.write(`<li>${element}</li>`);
+    }
+    document.write('</ul>');
+}
+primElements ([2, 17, 386, 'hello', 'world', true]);
