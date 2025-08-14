d1f37538b214e6b1ec04d1d55c61355d8fd836ba
Sem-mi01 <semmik.ys@gmail.com>
2025-08-14 20:14:27 +0400

    made task6-10.html, task6_10.ts

--- homework/homework6/homework6-10/task6_10.ts
+++ homework/homework6/homework6-10/task6_10.ts
@@ -28,13 +28,14 @@
 //
 // }
 
-const cardSuits = ['spade', 'diamond','heart', 'clubs'];
-const values = ['6', '7', '8', '9', '10', 'ace','jack','queen','king'];
+const cardSuits:string[] = ['spade', 'diamond','heart', 'clubs'];
+const values: string [] = ['6', '7', '8', '9', '10', 'ace','jack','queen','king'];
 
-const cards = [];
+type Card = {cardSuit: string, value: string, color?: string};
+const cards: Card[] = [];
 for (const cardSuit of cardSuits ) {
     for (const value of values) {
-        const card = {cardSuit: cardSuit, value: value };
+        const card: Card = {cardSuit: cardSuit, value: value};
         if (cardSuit === 'diamond' || cardSuit === 'heart') {
             card.color = 'red';
         } else {
@@ -50,4 +51,4 @@ console.log(cards.find(card => card.value === 'ace' && card.cardSuit === 'spade'
 console.log(cards.filter(card => card.value === '6'));
 console.log(cards.filter(card => card.color === 'red'));
 console.log(cards.filter(card => card.cardSuit === 'diamond'));
-console.log(cards.filter(card => (card.value !== '6' || card.value !== '7' || card.value !== '8' || card.value !== '9') && card.cardSuit === 'clubs'));
+console.log(cards.filter(card => card.cardSuit === 'clubs'  && (card.value !== '6'  &&  card.value !== '7'  &&  card.value !== '8'  &&  card.value !== '9')));
