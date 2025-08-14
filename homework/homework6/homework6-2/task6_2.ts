fce861b5a66b3e745b9080a85a537b1d94736cf2
Sem-mi01 <semmik.ys@gmail.com>
2025-08-14 16:46:10 +0400

    made task6-2.html, task6_2.ts

--- homework/homework6/homework6-2/task6_2.ts
+++ homework/homework6/homework6-2/task6_2.ts
@@ -3,8 +3,11 @@
 // – Перевести до великого регістру наступні стрінгові значення
 //
 //       ‘hello world’, ‘lorem ipsum’, ‘javascript is cool’
-let s1 = 'hello world';
-let s2 = 'lorem ipsum';
-let s3 = 'javascript is cool';
+// @ts-ignore
+let s1: string = 'hello world';
+// @ts-ignore
+let s2: string = 'lorem ipsum';
+// @ts-ignore
+let s3: string = 'javascript is cool';
 console.log(s1.toUpperCase() + ';' + s2.toUpperCase() + ';' + s3.toUpperCase() + ';');
 
