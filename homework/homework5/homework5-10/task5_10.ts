98719fe50abe8ee7234c6a55d1013ce7081207d1
Sem-mi01 <semmik.ys@gmail.com>
2025-08-14 15:48:00 +0400

    add homework5, task5-10.html, task5_10.ts

---
+++ homework/homework5/homework5-10/task5_10.ts
@@ -0,0 +1,17 @@
+// #pghbnSB
+//
+// – створити стрілочну функцію яка повертає найменьше число з масиву
+
+// @ts-ignore
+const minNumber = (numbers: number[]): number => {
+    let min = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        let number = numbers[i];
+            if (number < min) {
+                min = number;
+            }
+    }
+    return min;
+}
+
+console.log(minNumber([ 5, 12, -35, 98, -60]));
