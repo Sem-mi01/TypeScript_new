0b1ac8e4e8cf1025f4d945edeb8fe9a6141010c3
Sem-mi01 <semmik.ys@gmail.com>
2025-08-14 13:53:54 +0400

    add homework5, task5-5.html, task5_5.ts

---
+++ homework/homework5/homework5-5/task5_5.ts
@@ -0,0 +1,11 @@
+// #59g0IsA
+//
+// – створити стрілочну функцію, яка створює параграф з текстом та виводить його через document.write. Текст задати через аргумент
+
+
+const madParagraph = (text: string): void => {document.write(`<p>${text}</p>`)}
+
+madParagraph('lorem  ipsum') ;
+
+
+
