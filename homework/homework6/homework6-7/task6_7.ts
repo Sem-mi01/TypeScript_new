a392db98af317c0c37c2a2e284b4a403c1349ca8
Sem-mi01 <semmik.ys@gmail.com>
2025-08-14 18:30:02 +0400

    made task6-7.html, task6_7.ts

--- homework/homework6/homework6-7/task6_7.ts
+++ homework/homework6/homework6-7/task6_7.ts
@@ -8,10 +8,11 @@
 //
 // sortNums(nums,’descending’) // [21,11,3]
 
-let nums = [11,21,3];
-function sortNums(nums,direction) {
+let nums: number[] = [11,21,3];
+function sortNums(nums: number[], direction: string): number[] | undefined{
     if (direction === 'ascending') return nums.sort((a, b) => a - b);
     if (direction === 'descending') return nums.sort((a, b) => b - a);
+    return undefined
 }
 
 console.log(sortNums (nums, 'ascending'));
