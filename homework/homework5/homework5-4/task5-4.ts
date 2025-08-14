1326617f51f2b8d07f2f5dba15456aded7537d87
Sem-mi01 <semmik.ys@gmail.com>
2025-08-14 13:48:42 +0400

    add homework5, task5-4.html, task5_4.ts

---
+++ homework/homework5/homework5-4/task5-4.ts
@@ -0,0 +1,12 @@
+// #SIdMd0hQ
+//
+// – створити cтрілочну функцію, яка приймає масив та виводить кожен його елемент
+
+
+
+const prinArray = (array: number[] | any []): void => {
+    for (const product of array) {
+        console.log(product);
+}
+} ;
+prinArray([12,22,33,55]);
