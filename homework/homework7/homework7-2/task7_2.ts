d6970205924419e2583b36872c79b60ae9dda49f
Sem-mi01 <semmik.ys@gmail.com>
2025-08-15 21:45:06 +0400

    made  task7-2.html, task7_2.ts

---
+++ homework/homework7/homework7-2/task7_2.ts
@@ -0,0 +1,38 @@
+// #2ikXsE2WiKZ
+//
+// – Взяти масив з  User[] з попереднього завдання, та відфільтрувати, залишивши тільки об’єкти з парними id (filter)
+
+// @ts-ignore
+class UserClass {
+
+    constructor(
+        private _id: number, private _name: string, private _surname: string, private _email: string, private _phone: string
+    ) {}
+    get id() { return this._id; }
+    get name() { return this._name; }
+    get surname() { return this._surname; }
+    get email() { return this._email; }
+    get phone() { return this._phone; }
+}
+// @ts-ignore
+let users: UserClass[] = [
+    new UserClass(4, 'Roman', 'Ivanenko', 'Ivanenko@gmail.com', '5674531'),
+    new UserClass(9,  'Katerina',  'Pruhodko', 'Pruhodko@gmail.com',  '5674532'),
+    new UserClass(3,  'Polina',  'Gnatenko',  'Gnatenko@gmail.com',  '5674533'),
+    new UserClass(1,  'Olersandr',  'Petrenko',  'Petrenko@gmail.com', '5674534'),
+    new UserClass(10,  'Volodumur', 'Kovalenko',  'Kovalenko@gmail.com', '5674535'),
+    new UserClass(6,  'Anna',  'Mukutenko',  'Mukutenko@gmail.com',  '5674536'),
+    new UserClass(7,  'Dmutro',  'Avramenko', 'Avramenko@gmail.com',  '5674537'),
+    new UserClass(8,  'Oksana',  'Kostenko',  'Kostenko@gmail.com',  '5674538'),
+    new UserClass(2,  'Svitlana',  'Stepanenko',  'Stepanenko@gmail.com', '5674539'),
+    new UserClass(5,  'Bogdan', 'Magdenko', 'Magdenko@gmail.com',  '56745310'),
+]
+
+// @ts-ignore
+const filterUsers: UserClass = users.filter((user: UserClass) => user.id % 2 === 0);
+
+
+console.log(filterUsers);
+
+
+
