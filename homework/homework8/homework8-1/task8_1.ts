11c5881344161aeec45ccaa9174f5ba1d2d5d5b7
Sem-mi01 <semmik.ys@gmail.com>
2025-08-20 17:05:14 +0300

    made homework8 task8_1.ts task8-1.html

---
+++ homework/homework8/homework8-1/task8_1.ts
@@ -0,0 +1,47 @@
+// #iz6emEsP2BA
+//
+// – є масив
+//
+// let coursesAndDurationArray = [
+//
+//     {title: ‘JavaScript Complex’, monthDuration: 5},
+//
+// {title: ‘Java Complex’, monthDuration: 6},
+//
+// {title: ‘Python Complex’, monthDuration: 6},
+//
+// {title: ‘QA Complex’, monthDuration: 4},
+//
+// {title: ‘FullStack’, monthDuration: 7},
+//
+// {title: ‘Frontend’, monthDuration: 4}
+//
+// ];
+//
+// за допомогою map перетворити кожен елемент на наступний тип {id,title,monthDuration
+//
+//     Зробити все ВИКЛЮЧНО за допомогою інлайн конструкції
+
+interface ICoursesAndDurationArray {
+    title: string,
+    monthDuration: number
+}
+
+const courses: ICoursesAndDurationArray [] = [
+
+{title: 'JavaScript Complex', monthDuration: 5},
+
+{title: 'Java Complex', monthDuration: 6},
+
+{title: 'Python Complex', monthDuration: 6},
+
+{title: 'QA Complex', monthDuration: 4},
+
+{title: 'FullStack', monthDuration: 7},
+
+{title: 'Frontend', monthDuration: 4}
+
+];
+
+
+console.log(courses.map((course, index) => ({ id: index + 1, ...course})));
